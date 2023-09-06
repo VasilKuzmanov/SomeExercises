8ae3c7440589fe2379a8da5224d59f14c14d5665
VasilKuzmanov <118282723+VasilKuzmanov@users.noreply.github.com>
2023-09-06 22:56:28 +0300

    Update pawn_wars.py

--- pawn_wars.py
+++ pawn_wars.py
@@ -1,31 +1,34 @@
-def check_if_pawn_becomes_a_queen(colour, row, col):
-    if colour == "w" and row == 0 or colour == "b" and row == 7:
-        pawn_colour = "White" if colour == "w" else "Black"
-        quit(print(f"Game over! {pawn_colour} pawn is promoted to a queen at {chr(97 + col)}{8 - row}."))
+def check_if_pawn_becomes_a_queen(pawn, row, col, colour):
+    if pawn == "w" and row == 0 or pawn == "b" and row == 7:
+        quit(print(f"Game over! {colour} pawn is promoted to a queen at {chr(97 + col)}{8 - row}."))
 
 
-def move_pawn(colour, row, col):
-    def colour_is_white():
-        if chess_board[row - 1][col - 1] == "b" or chess_board[row - 1][col + 1] == "b":
-            quit(print(f"Game over! White win, capture on {chr(97 + chess_board[row - 1].index('b'))}{8 - row + 1}."))
-        chess_board[row - 1][col], chess_board[row][col] = "w", "-"
+def move_pawn(pawn, row, col, colour):
+    one_row_forward = row - 1 if pawn == "w" else row + 1
+    enemy_pawn = {"row_idx": None, "col_idx": None}
+    plus_one_minus_one = 1
 
-    def colour_is_black():
-        if chess_board[row + 1][col - 1] == "w" or chess_board[row + 1][col + 1] == "w":
-            quit(print(f"Game over! Black win, capture on {chr(97 + chess_board[row + 1].index('w'))}{8 - row - 1}."))
-        chess_board[row + 1][col], chess_board[row][col] = "b", "-"
+    for _ in range(2):
+        if chess_board[one_row_forward][col + plus_one_minus_one] != "-":
+            enemy_pawn["row_idx"] = one_row_forward
+            enemy_pawn["col_idx"] = col + plus_one_minus_one
 
-    colour_is_white() if colour == "w" else colour_is_black()
+        plus_one_minus_one *= -1
 
+    if enemy_pawn["row_idx"]:
+        quit(print(f"Game over! {colour} win, capture on {chr(97 + enemy_pawn['col_idx'])}{8 - one_row_forward}."))
+
+    chess_board[one_row_forward][col], chess_board[row][col] = pawn, "-"
 
 def main(last_played="b"):
     for i in range(64):
         row, col = divmod(i, 8)
         if chess_board[row][col] in "wb" and chess_board[row][col] != last_played:
-            pawn_colour = chess_board[row][col]
-            check_if_pawn_becomes_a_queen(pawn_colour, row, col)
-            move_pawn(pawn_colour, row, col)
-            main(pawn_colour)
+            pawn = chess_board[row][col]
+            colour = "White" if chess_board[row][col] == "w" else "Black"
+            check_if_pawn_becomes_a_queen(pawn, row, col, colour)
+            move_pawn(pawn, row, col, colour)
+            main(pawn)
 
 
 chess_board = [[x for x in input().split() + ["-"]] for _ in range(8)]
