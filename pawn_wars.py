94cc7649b29150d770271fc1abbae7235c0ffd7c
VasilKuzmanov <118282723+VasilKuzmanov@users.noreply.github.com>
2023-09-07 00:36:54 +0300

    Update pawn_wars.py

--- pawn_wars.py
+++ pawn_wars.py
@@ -19,15 +19,15 @@ def move_pawn(pawn, row, col, colour):
         quit(print(f"Game over! {colour} win, capture on {chr(97 + enemy_pawn['col_idx'])}{8 - one_row_forward}."))
 
     chess_board[one_row_forward][col], chess_board[row][col] = pawn, "-"
-    
+
 
 def main(last_played="b"):
     for i in range(64):
         row, col = divmod(i, 8)
-        
+
         if chess_board[row][col] in "wb" and chess_board[row][col] != last_played:
             pawn = chess_board[row][col]
-            colour = "White" if chess_board[row][col] == "w" else "Black"
+            colour = "White" if pawn == "w" else "Black"
             check_if_pawn_becomes_a_queen(pawn, row, col, colour)
             move_pawn(pawn, row, col, colour)
             main(pawn)
