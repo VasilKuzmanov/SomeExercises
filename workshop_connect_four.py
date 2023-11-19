3ffe9b14a7861f31af96e1cd6a357465156174b8
VasilKuzmanov <118282723+VasilKuzmanov@users.noreply.github.com>
2023-11-19 17:30:33 +0200

    Update workshop_connect_four.py

--- workshop_connect_four.py
+++ workshop_connect_four.py
@@ -1,5 +1,5 @@
 def print_board():
-    [print(f"[ {', '.join(row)} ]") for row in board]
+    [print(f"[{', '.join(row)}]") for row in board]
 
 
 def place_on_the_board(col_idx: int, player_symbol: str, row_idx=-1):
@@ -10,11 +10,11 @@ def place_on_the_board(col_idx: int, player_symbol: str, row_idx=-1):
     return ROWS + row_idx
 
 
-def check_for_win(row: int, col: int,):
+def check_for_win(row: int, col: int, ):
     check_list = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0}
 
     directions = ((1, 0), (0, -1), (0, 1), (-1, -1), (1, 1), (-1, 1), (1, -1))
-        # (bottom), (left), (right), (top-left), (bottom-right), (top-right), (bottom-left)
+    # (bottom), (left), (right), (top-left), (bottom-right), (top-right), (bottom-left)
 
     for index, direction in enumerate(directions):
         checked_row = row + direction[0]
@@ -22,19 +22,18 @@ def check_for_win(row: int, col: int,):
 
         while 0 <= checked_row < ROWS and 0 <= checked_col < COLS and \
                 board[row][col] == board[checked_row][checked_col]:
-
             check_list[index] += 1
             checked_row += direction[0]
             checked_col += direction[1]
 
     if counter_for_draw == ROWS * COLS:
         print_board()
-        quit(print('\033[92m' + "Draw!" + '\033[0m'))
+        quit(print('\033[92m' + "Draw!"))
 
     if check_list[0] == 3 or check_list[1] + check_list[2] >= 3 or \
-        check_list[3] + check_list[4] >= 3 or check_list[5] + check_list[6] >= 3:
+            check_list[3] + check_list[4] >= 3 or check_list[5] + check_list[6] >= 3:
         print_board()
-        quit(print('\033[92m' + f"Player {current_player} " + '\033[92m' +  "wins!" + '\033[0m'))
+        quit(print('\033[92m' + f"Player {current_player} " + '\033[92m' + "wins!"))
 
 
 ROWS, COLS = 6, 7
@@ -44,7 +43,6 @@ counter_for_draw = 0
 board = [["0"] * COLS for _ in range(ROWS)]
 
 current_player, second_player = '\x1b[33m1\x1b[39m', '\x1b[31m2\x1b[39m'
-
 
 while True:
 
